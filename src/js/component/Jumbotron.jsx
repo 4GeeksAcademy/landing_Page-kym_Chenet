8c1771148193ed423451812569ac8c81f414d380
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-09-06 19:32:38 +0000

    Avance

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+    return (
+        <div class="jumbotron">
+        <h1 class="display-4">A Warm Welcome!</h1>
+        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem saepe labore veniam commodi repudiandae, ratione necessitatibus quasi doloribus iure. Necessitatibus expedita veritatis obcaecati repudiandae libero. Vero possimus officia exercitationem ut.</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+        </p>
+      </div>
+    );
+};
+
+export default Jumbotron;
